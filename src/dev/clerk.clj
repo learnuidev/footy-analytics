5e5eac60be508be0a60b3996bc59eea3d8f530cd
learnuidev <learnuidev@gmail.com>
2022-03-31 14:36:43 -0400

    Added data sets from clerk demo

--- src/dev/clerk.clj
+++ src/dev/clerk.clj
@@ -1,7 +1,22 @@
 (ns clerk
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.data.json :as json]
+            [clojure.data.csv :as csv]
+            [clojure.pprint :as pprint]
+            [clojure.java.io :as io]
+            [nextjournal.clerk :as clerk]))
+
+;;
+;; 1. Find all competitions
+(defn find-all-competitions  []
+  (json/read-str (slurp (str "open-data/data/competitions.json"))
+                 :key-fn keyword))
+
+(comment
+  (count (find-all-competitions)))
 
 
 (comment
   (clerk/serve! {:browse? true})
-  (clerk/show! "notebooks/rule_30.clj"))
+  (clerk/table (find-all-competitions))
+  (clerk/show! "notebooks/rule_30.clj")
+  (clerk/show! "notebooks/data_science.clj"))
